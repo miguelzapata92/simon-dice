1c632639c16a89511f70dd4a97648549420c804f
miguelzapata92 <71832311+miguelzapata92@users.noreply.github.com>
2021-08-03 22:41:09 -0300

    Agregando set time out y funciones de callback

--- main.js
+++ main.js
@@ -1,6 +1,48 @@
 let secuenciaMaquina = [];
 let secuenciaUsuario = [];
-ronda = 0
+let ronda = 0
+const $btnComenzar = document.querySelector("#btnComenzar")
+$btnComenzar.onclick = comenzarJuego;
 
-document.querySelector('button[type=button]').onclick = comenzarJuego;
 
+function comenzarJuego() {
+    manejarRonda();
+    cambiarEstado();
+}
+
+function manejarRonda() {
+    bloquearBoton();
+    $cuadroAleatorio = obtenerCuadroAleatorio();
+    secuenciaMaquina.push($cuadroAleatorio);
+
+    function bloquearBoton() {
+        $btnComenzar.disabled = true;
+    }
+
+    const RETRASO_TURNO_USUARIO = (secuenciaMaquina.length + 1) * 1000;
+
+    secuenciaMaquina.forEach(function($cuadro, i) {
+        const RETRASO_MS = (i + 1) * 1000;
+        setTimeout(function() {
+            resaltar($cuadro);
+        }, RETRASO_MS);
+    });
+
+    setTimeout(function() {
+        document.querySelector("#estado").innerText = "Es tu turno!";
+        desbloquearInputUsuario();
+    }, RETRASO_TURNO_USUARIO);
+
+    secuenciaUsuario = [];
+    ronda++;
+    actualizarNumeroRonda();
+}
+
+
+function obtenerCuadroAleatorio() {
+
+}
+
+function actualizarNumeroRonda(ronda) {
+    document.querySelector("#ronda").textContent = ronda;
+}
